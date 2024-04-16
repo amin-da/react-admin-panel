30dcc509fedbaad002407a4e0d3bed9b973ad6ad
amin-da <darabniya@yahoo.com>
2024-04-16 18:23:26 +0330

    feat : compelte geography chart

--- src/pages/Geography.jsx
+++ src/pages/Geography.jsx
@@ -1,9 +1,24 @@
-
+import { tokens } from "../theme";
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material";
+import Header from "../components/Header";
+import GeoGraphyChart from "../components/charts/GeoGraphyChart";
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>Geography</div>
-  )
-}
+    <Box m="20px">
+      <Header title="GeoGraphy Chart" subtitle="Simple GeoGraphy Chart" />
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeoGraphyChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default Geography
+export default Geography;
